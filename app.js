07c62e1c7f347afa35532bdd25b9c1dd7f581eca
24LP1550 24LP1550 <24LP1550@G303-CC0149.local>
2025-01-03 12:33:29 +0800

    Javascript

---
+++ app.js
@@ -0,0 +1,280 @@
+// for(let i=6; i<=0; i--){
+//     let a=""
+//     for ( let j=6; j<=i; j--){
+//         a += "*"
+//     }
+//     console.log(a)
+// }
+
+//  for(let i=0; i<=100; i++)
+//     if (i%2==1)
+
+// a=0
+
+
+// for(let i=0; i<=100; i++){
+//     a=a+i
+// }
+// console.log(a)
+// var a=1
+// for(let i=1; i<=10; i++){ 
+//     if( i%2==0){
+//     console.log(i)
+//     a=a*i
+//     } 
+//  }
+//  console.log(a)
+
+// var a=1
+// for(let i=1; i<=100; i++){ 
+//     if( i%2==1){
+//     console.log(i)
+//     a=a*i
+//     } 
+//  }
+//  console.log(a)
+
+// var a=1
+// for(let i=1; i<=100; i++){ 
+//     if( i%2==0 &&  i%2==1 ){
+//     console.log(i)
+//     } 
+//  }
+
+//  var a=1 agiiiiii
+//  for(let i=1; i<=100; i++){ 
+//      if( i%7==0){
+//      console.log(i)
+//      } 
+//   }
+
+
+// var palindrome = 'hadgalagdah'
+//  console.log( palindrome.length, palindrome[i])
+
+// function palindrome (str){
+//     if ( str == "hadgaldah")
+// }
+
+// var a=1
+// var too=2
+// var zereg=4
+// for (let i =1; i<=zereg; i++) {
+//      a=a*too
+// }
+//  console.log(a);
+
+// var Prime = 2;
+// var result 
+// for(let i = 1; i<=100 ; i++){
+//     result = Prime/i
+//     console.log(result)
+//     if(Prime%i!=0){
+//         console.log("test")
+//     }
+// }
+
+
+// function Prime (n)
+// {
+//     let Prime = true;
+//     if(n==1) Prime= false
+//     for (let i=2; i<n; i++){
+//         if ( n %1==0 ){
+//             prime=false
+//         }
+//     }
+//     console.log(Prime)
+// }
+// console.log(2)
+
+// function Prime(n){
+//     let isPrimeNumber = n;
+
+//     if (n <= 1) isPrimeNumber = false
+
+//     for (let i = 2; i < n; i++) {
+//         if (n % i == 0) {
+//             isPrimeNumber = false
+//         }
+//     }
+//     console.log(isPrimeNumber)
+// }
+// // Prime(13)
+
+// for(let j=0; j<=100;j++){
+//     if(Prime(j) == true){
+
+//         console.log(j)
+//     }
+// }
+
+
+
+
+// const names = [
+//     "baljan",
+//     "itgel",
+//     "jamiyansuren",
+//     "chingun",
+//     "telmuun",
+//     "chinguun",
+//     "nomi",
+//     "aygul",
+//     "behbat",
+//     "narangerel",
+//     "oyunbat",
+//     "turbat",
+//     "baasandash",
+//     "jargalsaihan",
+//     "tugsbileg",
+//     "huwituguldur",
+//     "ursaa",
+//     "ider",
+//     "ariunjargal"
+// ];
+
+// var find= "baljan"
+// for(let i = 0; i < names.length; i++) {
+//     if(
+//         names[i]==find
+//     )
+//     {
+//         console.log(i)
+//     }
+// }
+
+
+// find = ""
+// function sss(find) {
+//     for (let i = 0; i < names.length; i++)
+//         if (names[i] == find) {
+//             return i
+//         }
+// }
+// console.log(sss("ursaa"))
+// console.log(sss("oyunbat"))
+
+// var number = [5, 13, 18, 9, 2, 6]
+// var max=0 
+// var Mindex=0
+//     for(let i=0; i<number.length; i++){
+//         if (max< number [i]){
+//            max=number [i]
+//            Mindex=i
+//         }
+        
+//     }
+//     console.log(Mindex)
+    
+
+// let a =3;
+// let b =9;
+// var diff= a-b
+// console.log(a+b-a/2)
+
+
+// var a=3
+// var b=220
+// var c=9
+// if(a>b && a>c)
+//     console.log(a)
+// else if(b>a && b>c)
+//     console.log(b)
+// else (c)
+//     console.log(c)
+
+// var number = [5, 13, 9, 18]
+// var max=0 
+// var Mindex=0
+//     for(let i=0; i<number.length; i++){
+//         if (max< number [i]){
+//            max=number [i]
+//                   }
+        
+//     }
+//     console.log(Mindex)
+    
+
+// var a=9
+// var b=7
+// var c=12
+// var d=89
+// if (a>b && a>c && a>d){
+//     console.log(a)}
+// else if (a>b && a>c && a>d && a<c){
+//     console.log(a)}
+// else if (a>b && a>c && a>d && a<d){
+//     console.log(a)
+// }
+// else if (b>c && b>a && b>d && b<d){
+//     console.log(b)
+// }
+// else if (b>c && b>a && b>d && b<a){
+//     console.log(b)
+// }
+// else if (b>c && b>a && b>d && b<c){
+//     console.log(b)
+// }
+// else if (c>a && c>b && c>d && c<d) {
+//     console.log(c)
+// }
+// else if (c>a && c>b && c>d && c<a) {
+//     console.log(c)
+// }
+// else if (c>a && c>b && c>d && c<b) {
+//     console.log(c)
+// }
+// else if (d>a && d>b && d>c && d<c) {
+//     console.log(d)
+// }
+// else if (d>a && d>b && d>c && d<a) {
+//     console.log(d)
+// }
+// else if (d>a && d>b && d>c && d<b) {
+//     co
+//     cnsole.log(d)
+// }
+
+// var a=9
+// var b=7
+// var c=99
+// var d=89
+// if (a>b && a>c && a<d){
+//     console.log(a)}
+// else if (a>b && a>d && a<d){
+//         console.log(a)}
+// else if (a>c && a>d && a<c){
+//             console.log(a)}
+// else if (b>a && b>d && b<c){
+//     console.log(b)}
+// else if (b>a && b>c && b<d){
+//         console.log(b)}
+// else if (b>c && b>d && b<a){
+//             console.log(b)}
+// else if (c>a && c>b && c<d ) {
+//     console.log(c)}
+// else if (c>b && c>d && c<a) {
+//         console.log(c)}
+// else if (c>d && c>a && c<b) {
+//         console.log(c)}   
+// else if (d>a && d>b && d<c) {
+//     console.log(d)
+// }
+// else if (d>a && d>c && d<b) {
+//     console.log(d)
+// }
+// else if (d>b && d>c && d<a) {
+//     console.log(d)
+// }
+
+
+// const nums = [2, 7, 7, 15]
+// for(let i=0; i<nums.length; i++){
+//     for(let k=i+1; k<nums.length; k++){
+//         if(nums[k]==nums[i]){
+//             console.log(nums [i] )
+//         }
+//     }
+// }
+        
